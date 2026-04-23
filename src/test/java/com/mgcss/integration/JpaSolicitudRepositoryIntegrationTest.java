d8f24fdbf90d5fce40857ac0aed94f243122d82c
Jaime Garcia Marin <jaime.garcia1@alu.uhu.es>
2026-04-23 12:35:29 +0200

    fix: pom

--- src/test/java/com/mgcss/integration/JpaSolicitudRepositoryIntegrationTest.java
+++ src/test/java/com/mgcss/integration/JpaSolicitudRepositoryIntegrationTest.java
@@ -3,11 +3,9 @@ package com.mgcss.integration;
 
 import com.mgcss.domain.MgcssTrackL2Grupo4Application;
 import com.mgcss.domain.Solicitud;
-
 import com.mgcss.infraestructure.persistence.JpaSolicitudRepository;
 import com.mgcss.infraestructure.persistence.SolicitudEntity;
 
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.junit.jupiter.api.Tag;
@@ -15,6 +13,7 @@ import org.junit.jupiter.api.Tag;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
