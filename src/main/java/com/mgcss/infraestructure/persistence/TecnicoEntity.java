516f08d370699b1a75cb9a25b959d1d9967a6c30
Jaime Garcia Marin <jaime.garcia1@alu.uhu.es>
2026-04-23 14:16:26 +0200

    fix: cambiando getter and seter para usar lombok

--- src/main/java/com/mgcss/infraestructure/persistence/TecnicoEntity.java
+++ src/main/java/com/mgcss/infraestructure/persistence/TecnicoEntity.java
@@ -1,36 +1,23 @@
 package com.mgcss.infraestructure.persistence;
+
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class TecnicoEntity {
-	@Id
+	
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private boolean activo;
 
-    
-	public TecnicoEntity() {
-		
-	}
-
-	public TecnicoEntity(boolean activo) {
-		this.activo = activo;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public boolean isActivo() {
-		return activo;
-	}
-
-	public void setActivo(boolean activo) {
-		this.activo = activo;
-	}
+    public TecnicoEntity(boolean activo) {
+        this.activo = activo;
+    }
 }
