048df4595fe345b1725ce58e354801ab552bfa76
Jaime Garcia Marin <jaime.garcia1@alu.uhu.es>
2026-04-23 14:21:47 +0200

    fix: coverange bajo

--- src/main/java/com/mgcss/infraestructure/persistence/SolicitudEntity.java
+++ src/main/java/com/mgcss/infraestructure/persistence/SolicitudEntity.java
@@ -2,16 +2,12 @@ package com.mgcss.infraestructure.persistence;
 
 import jakarta.persistence.*;
 import java.time.LocalDate;
+
 import com.mgcss.domain.Solicitud;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
 public class SolicitudEntity {
-	
-    @Id 
+	@Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -22,5 +18,31 @@ public class SolicitudEntity {
     
     @ManyToOne 
     private TecnicoEntity tecnico;
+
+    
+    public Long getId() { 
+    	return id; 
+    }
+    public void setId(Long id) { 
+    	this.id = id; 
+    }
+    public LocalDate getFechaCreacion() {
+		return fechaCreacion;
+	}
+	public void setFechaCreacion(LocalDate fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+	public Solicitud.Estado getEstado() {
+		return estado;
+	}
+	public void setEstado(Solicitud.Estado estado) {
+		this.estado = estado;
+	}
+	public TecnicoEntity getTecnico() {
+		return tecnico;
+	}
+	public void setTecnico(TecnicoEntity tecnico) {
+		this.tecnico = tecnico;
+	}    
     
 }
--- src/main/java/com/mgcss/infraestructure/persistence/TecnicoEntity.java
+++ src/main/java/com/mgcss/infraestructure/persistence/TecnicoEntity.java
@@ -1,23 +1,36 @@
 package com.mgcss.infraestructure.persistence;
-
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
 public class TecnicoEntity {
-	
-    @Id
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private boolean activo;
 
-    public TecnicoEntity(boolean activo) {
-        this.activo = activo;
-    }
+    
+	public TecnicoEntity() {
+		
+	}
+
+	public TecnicoEntity(boolean activo) {
+		this.activo = activo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public boolean isActivo() {
+		return activo;
+	}
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
 }
